fbc32bcf98c10ff1caaf5b9710bec12e9788e726
Ifeoluwa <ifel1939@gmail.com>
2024-04-17 22:23:07 +0100

    using mock function to test

--- test/src/__test__/UserForm.test.jsx
+++ test/src/__test__/UserForm.test.jsx
@@ -1,31 +1,38 @@
-import { screen,render } from "@testing-library/react";
-import user from "@testing-library/user-event"
+import { screen, render } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import UserForm from "../UserForm";
 
+describe("Testing the userform component", () => {
+  test("It should show two inputs and a button", () => {
+    render(<UserForm />);
 
-describe("Testing the userform component",()=>{
+    const inputs = screen.getAllByRole("textbox");
 
+    const button = screen.getByRole("button");
 
+    expect(inputs).toHaveLength(2);
+    expect(button).toBeInTheDocument();
+  });
 
-test("It should show two inputs and a button",()=>{
+  test("the function should be called and has the rigth arguments", () => {
+    const mockFunction = jest.fn();
+    render(<UserForm onUserAdd={mockFunction} />);
 
+    const [inputName, inputEmail] = screen.getAllByRole("textbox");
 
-render(<UserForm/>)
+    user.click(inputName);
+    user.keyboard("me");
 
-const inputs=screen.getAllByRole("textbox")
+    user.click(inputEmail);
+    user.keyboard("me@gmail.com");
 
-const button = screen.getByRole("button");
+    const button = screen.getByRole("button");
+    user.click(button);
 
-
-
-expect(inputs).toHaveLength(2)
-expect(button).toBeInTheDocument()
-
-
-
-})
-
-
-
-
-})
+    expect(mockFunction).toHaveBeenCalled();
+    expect(mockFunction).toHaveBeenCalledWith({
+      name: "me",
+      email: "me@gmail.com",
+    });
+  });
+});
