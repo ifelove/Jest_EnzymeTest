77045ed21c686c3cca04748fbc5adc6703bdd903
Ifeoluwa <ifel1939@gmail.com>
2024-04-18 20:00:21 +0100

    testing the user featurres

--- test/src/__test__/UserInput.test.jsx
+++ test/src/__test__/UserInput.test.jsx
@@ -1,38 +1,33 @@
-import { screen,render,within } from "@testing-library/react";
-import Userinput from "../UserInput"
-
-
-
+import { screen, render, within } from "@testing-library/react";
+import Userinput from "../UserInput";
 
 describe(" Testing the userlist ", () => {
+  const users = [
+    { name: "me", email: "me@gmail" },
+    { name: "you", email: "you@gmail" },
+  ];
   test("render one per user", () => {
-const users = [
-  { name: "me", email: "me@gmail" },
-  { name: "you", email: "you@gmail" },
-];
-//render component
-const {container}=render(<Userinput users={users}/>)
+    //render component
+    const { container } = render(<Userinput users={users} />);
 
+    //screen.logTestingPlaygroundURL()
 
-//screen.logTestingPlaygroundURL()
+    //const rows=within(screen.getByTestId('users')).getAllByRole("row")
 
+    const rows = container.querySelectorAll("tbody tr");
 
-//const rows=within(screen.getByTestId('users')).getAllByRole("row")
-
-const rows=container.querySelectorAll("tbody tr")
-
-expect(rows).toHaveLength(2)
-
-
-
-
+    expect(rows).toHaveLength(2);
   });
 
   test("render the name and email of the user", () => {
+    render(<Userinput users={users} />);
 
+    users.forEach((user) => {
+      const name = screen.getByRole("cell", { name: user.name });
+      const email = screen.getByRole("cell", { name: user.email });
 
-
-
-
+      expect(name).toBeInTheDocument();
+      expect(email).toBeInTheDocument();
+    });
   });
 });
